3ebc886cdb57b9566ce7b28c08767899857df2b7
jcost035 <jcost035@fiu.edu>
2024-11-25 03:55:33 -0500

    frontend first commit

---
+++ client/jorgescout/components/EditScreenInfo.tsx
@@ -0,0 +1,96 @@
+import React, {useEffect, useState} from 'react';
+import { StyleSheet } from 'react-native';
+
+import { ExternalLink } from './ExternalLink';
+import { MonoText } from './StyledText';
+import { Text, View } from './Themed';
+
+import Colors from '@/constants/Colors';
+
+import { getReading } from '@/scripts/scripts.ts';
+
+export default function EditScreenInfo({ path }: { path: string }) {
+  const [reading, setReading] = useState<string>('Loading...');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getReading(); // Resolve the Promise
+        setReading(data); // Update the state with the resolved data
+      } catch (error) {
+        console.error('Error fetching reading:', error);
+        setReading('Error fetching reading'); // Handle errors gracefully
+      }
+    };
+
+    fetchData(); // Call the async function
+  }, []); // Empty dependency array ensures this runs only once when the component mounts
+
+
+  return (
+    <View>
+      <View style={styles.getStartedContainer}>
+        <Text
+          style={styles.getStartedText}
+          lightColor="rgba(0,0,0,0.8)"
+          darkColor="rgba(255,255,255,0.8)">
+          Blood sufar is:
+        </Text>
+
+        <View
+          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
+          darkColor="rgba(255,255,255,0.05)"
+          lightColor="rgba(0,0,0,0.05)">
+          <MonoText>{reading}</MonoText>
+        </View>
+
+        <Text
+          style={styles.getStartedText}
+          lightColor="rgba(0,0,0,0.8)"
+          darkColor="rgba(255,255,255,0.8)">
+          :)
+        </Text>
+      </View>
+
+      <View style={styles.helpContainer}>
+        <ExternalLink
+          style={styles.helpLink}
+          href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet">
+          <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
+            Tap here if your app doesn't automatically update after making changes
+          </Text>
+        </ExternalLink>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightContainer: {
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  helpContainer: {
+    marginTop: 15,
+    marginHorizontal: 20,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    textAlign: 'center',
+  },
+});
