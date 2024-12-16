05c6142aa9b28dfe0393d14d2a8181fd66a8e81b
jcost035 <jcost035@fiu.edu>
2024-12-16 17:04:08 -0500

    tryig soehintg

--- server/config.py
+++ server/config.py
@@ -4,3 +4,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:012106@localhost/jorgescout')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SCHEDULER_API_ENABLED = True
+    SQLALCHEMY_ENGINE_OPTIONS = {"connect_args": {"sslmode": "disable"}}
